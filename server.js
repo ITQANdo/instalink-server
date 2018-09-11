ce1b682b3e782a138964e4737133691871c599dc
Ahmed Nefzaoui <nefzaoui.ahmed@hotmail.com>
2018-09-11 15:34:48 +0100

    Upgraded instalink module; and made changes to resver accordingly

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const instalink = require('./instalink');
 const app = express();
 const port = process.env.INSTALINK_PORT || 3300;
 
-
 app.listen(port);
 
 app.get("/p/:id", (req, res, next) => {
-  let postID = req.params.id;
-  instalink.getURL(postID).then((r) => {
+  async function fetchImageInfo() {
+    await instalink.init();
+    let r = await instalink.imageInfo(req.params.id);
+    await instalink.terminate();
+    return r;
+  }
+
+  fetchImageInfo().then((r) => {
     res.json(r);
   });
-});
 
+});
 
 console.log(`We're running on port: ${port}`);
