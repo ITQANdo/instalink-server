843f6c84e7d5b5430d84c42db9c52ce31e121c00
Ahmed Nefzaoui <nefzaoui.ahmed@hotmail.com>
2018-09-12 19:20:15 +0100

    Design Overhaul + Adapt to new APIs + Updated Instalink Module to latest version.

--- public/js/main.js
+++ public/js/main.js
@@ -2,65 +2,87 @@
 
 function fetchPost(self) {
 
+  var submitButton = document.querySelector('.submit');
   var inputUrl = document.getElementById('post_url').value;
-  var idFromInput = inputUrl.substr(inputUrl.indexOf('p/') + 2);
-  var xmlhttp;
-  console.log(idFromInput);
-
-  var url = "/p/" + idFromInput;
-
-  if (window.XMLHttpRequest) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp = new XMLHttpRequest();
+  if (inputUrl === "") {
+    console.log(`You didn't enter a URL.`);
   } else {
-    // code for IE6, IE5
-    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    var spinner = document.querySelector('.loading');
+    spinner.style.display = "block";
+    var idFromInput = inputUrl.substr(inputUrl.indexOf('p/') + 2);
+    var xmlhttp;
+    console.log(idFromInput);
+
+    var url = "/p/" + idFromInput;
+
+    if (window.XMLHttpRequest) {
+      // code for IE7+, Firefox, Chrome, Opera, Safari
+      xmlhttp = new XMLHttpRequest();
+    } else {
+      // code for IE6, IE5
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.addEventListener("load", transferComplete);
+    xmlhttp.addEventListener("error", transferFailed);
+    xmlhttp.addEventListener("abort", transferCanceled);
+
+    xmlhttp.open("get", url, true);
+    xmlhttp.send();
+
+    // return xmlhttp.responseText;
+
+    function transferComplete(evt) {
+      console.log("The transfer is complete.");
+      console.log(JSON.parse(xmlhttp.response));
+      buildResult(JSON.parse(xmlhttp.response))
+    }
+
+    function transferFailed(evt) {
+      console.log("An error occurred while transferring the file.");
+    }
+
+    function transferCanceled(evt) {
+      console.log("The transfer has been canceled by the user.");
+    }
+
   }
-  xmlhttp.addEventListener("load", transferComplete);
-  xmlhttp.addEventListener("error", transferFailed);
-  xmlhttp.addEventListener("abort", transferCanceled);
 
-  xmlhttp.open("get", url, true);
-  xmlhttp.send();
+  function buildResult(obj) {
+    let postStatusHolder = document.getElementById('postStatusHolder');
+    let postMessageHolder = document.getElementById('postMessageHolder');
+    let postTypeHolder = document.getElementById('postTypeHolder');
+    let userIDholder = document.getElementById('userIDholder');
+    let rawURLsholder = document.getElementById('rawURLsholder');
+    let postDescHolder = document.getElementById('postDescHolder');
+    let postLikesHolder = document.getElementById('postLikesHolder');
+    let postVideoLikesHolder = document.getElementById('postVideoLikesHolder');
+    let postVideoViewsHolder = document.getElementById('postVideoViewsHolder');
 
-  // return xmlhttp.responseText;
+    postStatusHolder.textContent = obj.STATUS;
+    postMessageHolder.textContent = obj.MESSAGE
 
-  function transferComplete(evt) {
-    console.log("The transfer is complete.");
-    console.log(JSON.parse(xmlhttp.response));
-    buildResult(JSON.parse(xmlhttp.response))
-  }
+    if (obj.STATUS === "OK") {
+      postTypeHolder.textContent = obj.TYPE;
+      userIDholder.textContent = obj.USERNAME;
+      let urlsDOM = '';
+      for (var i = 0; i < obj.URL.length; i++) {
+        urlsDOM += `<span><a class="buttonDownload" href="${obj.URL[i]}" target="_blank">Download ${i + 1}</a></span>&nbsp;&nbsp;`
+      }
+      rawURLsholder.innerHTML = urlsDOM;
+      postDescHolder.textContent = obj.DESCRIPTION;
+      postLikesHolder.textContent = obj.IMAGE_LIKES;
+      postVideoLikesHolder.textContent = obj.VIDEO_LIKES;
+      postVideoViewsHolder.textContent = obj.VIDEO_VIEWS;
 
-  function transferFailed(evt) {
-    console.log("An error occurred while transferring the file.");
-  }
-
-  function transferCanceled(evt) {
-    console.log("The transfer has been canceled by the user.");
-  }
-
-}
-
-function buildResult(obj) {
-  let postStatusHolder = document.getElementById('postStatusHolder');
-  let userIDholder = document.getElementById('userIDholder');
-  let postLikesHolder = document.getElementById('postLikesHolder');
-  let imageURLholder = document.getElementById('imageURLholder');
-  let postDescHolder = document.getElementById('postDescHolder');
-
-  postStatusHolder.textContent = obj.status;
-
-  if (obj.status === "OK") {
-    userIDholder.textContent = obj.USERNAME;
-    postLikesHolder.textContent = obj.LIKES;
-    imageURLholder.href = obj.URL;
-    imageURLholder.textContent = "Raw Image URL";
-    postDescHolder.textContent = obj.TXT;
-  } else {
-    userIDholder.textContent = "ERROR.";
-    postLikesHolder.textContent = "ERROR.";
-    imageURLholder.href = "#";
-    imageURLholder.textContent = "No Image URL. Error.";
-    postDescHolder.textContent = "ERROR...";
+    } else {
+      postTypeHolder.textContent = "No type, not a correct post.";
+      userIDholder.textContent = "No username found.";
+      rawURLsholder.innerHTML = "No URLs found";
+      postDescHolder.textContent = "No description found.";
+      postLikesHolder.textContent = "Error.";
+      postVideoLikesHolder.textContent = "Error.";
+      postVideoViewsHolder.textContent = "Error.";
+    }
+    spinner.style.display = "none";
   }
 }
